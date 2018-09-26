d217bc5cb7ed0dbaeb7a6142de7bddd5ee2e3556
Harald Lapp <h.lapp@clipdealer.de>
2018-09-26 12:07:44 +0200

    use file created by mktemp for identifiers

--- iadl.sh
+++ iadl.sh
@@ -32,24 +32,30 @@ cd "$3"
 
 echo "Downloading list of entries for collection name $1 ..."
 
-wget -O - -nd -q "https://archive.org/advancedsearch.php?q=$1&fl%5B%5D=identifier&sort%5B%5D=identifier+asc&sort%5B%5D=&sort%5B%5D=&rows=9999&page=1&callback=callback&save=yes&output=csv" 2>/dev/null | tail -n +2 | sed 's/"//g' > identifiers.txt
+tmp=$(mktemp 2>/dev/null || mktemp -t "tmp.XXXXXXXXXX")
+
+wget -O - -nd -q "https://archive.org/advancedsearch.php?q=$1&fl%5B%5D=identifier&sort%5B%5D=identifier+asc&sort%5B%5D=&sort%5B%5D=&rows=9999&page=1&callback=callback&save=yes&output=csv" 2>/dev/null \
+    | tail -n +2 \
+    | sed 's/"//g' > $tmp
 
 if [ ${PIPESTATUS[0]} -ne 0 ]; then
     echo "Error downloading identifiers."
     exit 1
 fi
 
-num=$(cat identifiers.txt | wc -l)
+num=$(cat $tmp | wc -l)
 
 if [ $num -eq 0 ]; then
     echo "No identifiers found for collection $1. Check name and try again."
-    rm identifiers.txt
+    rm $tmp
     exit 1
 fi
 
 echo "Beginning wget download of $num identifiers ..."
 
-wget -r -H -nc -np -nH -nd -e robots=off --domains archive.org --exclude-domains blog.archive.org -i identifiers.txt -B 'https://archive.org/download/' -A $2 1>/dev/null 2>&1
+wget -r -H -nc -np -nH -nd -e robots=off \
+    --domains archive.org --exclude-domains blog.archive.org \
+    -i $tmp -B 'https://archive.org/download/' -A $2 1>/dev/null 2>&1
 
 if ls -U *_text.pdf > /dev/null 2>&1; then
     echo "Found text-format PDFs, moving into text/ directory ..."
@@ -65,6 +71,6 @@ fi
 
 echo "Deleting temporary files ..."
 
-rm identifiers.txt
+rm $tmp
 
 echo "Complete."
